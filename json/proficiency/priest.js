db62f3e26d232aa0d352b3499e94594f142831ad
bingeboy <design@jpmcgarrity.com>
2013-06-06 22:20:10 -0400

    adding json proficiency files

---
+++ json/proficiency/priest.js
@@ -0,0 +1,296 @@
+[
+    {
+        "misc": "SM", 
+        "modifier": "+1", 
+        "points": "1", 
+        "stat": "INTELLIGENCE", 
+        "type": "Administration "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "CHARISMA", 
+        "type": "Alms "
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "-2", 
+        "points": "1 or 2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Ancient Geography"
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-1", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Ancient History"
+    }, 
+    {
+        "misc": "CN", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Anatomy"
+    }, 
+    {
+        "misc": "", 
+        "modifier": " 0", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Astrology"
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE", 
+        "type": "Bookbinding "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Bureaucracy"
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "WISDOM", 
+        "type": "Ceremony "
+    }, 
+    {
+        "misc": "Pn", 
+        "modifier": "-1", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Diagnostics"
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "-1", 
+        "points": "1", 
+        "stat": "CHARISMA", 
+        "type": "Diplomacy "
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "-1", 
+        "points": "2", 
+        "stat": "WISDOM ", 
+        "type": "Dream Interpretation "
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-3", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Engineering"
+    }, 
+    {
+        "misc": "AQ", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Genie Lore "
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-2", 
+        "points": "2", 
+        "stat": "WISDOM ", 
+        "type": "Healing"
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-2", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Herbalism"
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "INTELLIGENCE", 
+        "type": "Investigation "
+    }, 
+    {
+        "misc": "", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Languages, Ancient "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Law"
+    }, 
+    {
+        "misc": "", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "CHARISMA ", 
+        "type": "Local History"
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Navigation "
+    }, 
+    {
+        "misc": "CN", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Necrology"
+    }, 
+    {
+        "misc": "CN", 
+        "modifier": "-3", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Netherworld Knowledge"
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Omen Interpretation"
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "WISDOM", 
+        "type": "Omen Reading "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE", 
+        "type": "Papermaking "
+    }, 
+    {
+        "misc": "PW", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Planology"
+    }, 
+    {
+        "misc": "", 
+        "modifier": "+1", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Reading/Writing"
+    }, 
+    {
+        "misc": "", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Religion "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "-2", 
+        "points": "2", 
+        "stat": "INTELLIGENCE", 
+        "type": "Sage Knowledge "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "+1", 
+        "points": "1", 
+        "stat": "DEXTERITY", 
+        "type": "Scribe "
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "+1", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Shamanic Ritual"
+    }, 
+    {
+        "misc": "Bb", 
+        "modifier": " 0", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Soothsaying"
+    }, 
+    {
+        "misc": "", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Spellcraft "
+    }, 
+    {
+        "misc": "PW", 
+        "modifier": "-5", 
+        "points": "2", 
+        "stat": "INTELLIGENCE ", 
+        "type": "Spell Recovery "
+    }, 
+    {
+        "misc": "CN", 
+        "modifier": "-4", 
+        "points": "2", 
+        "stat": "CHARISMA ", 
+        "type": "Spirit Lore"
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "+2", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Spirit Lore"
+    }, 
+    {
+        "misc": "Sh", 
+        "modifier": "-3", 
+        "points": "2", 
+        "stat": "CONSTITUTION ", 
+        "type": "Trance "
+    }, 
+    {
+        "misc": "CS", 
+        "modifier": " 0", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Undead Knowledge "
+    }, 
+    {
+        "misc": "SM", 
+        "modifier": "-1", 
+        "points": "1", 
+        "stat": "INTELLIGENCE", 
+        "type": "Undead Lore "
+    }, 
+    {
+        "misc": "CN", 
+        "modifier": "-2", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Venom Handling "
+    }, 
+    {
+        "misc": "CR", 
+        "modifier": "-3", 
+        "points": "1", 
+        "stat": "WISDOM ", 
+        "type": "Vetinary Healing "
+    }
+]
